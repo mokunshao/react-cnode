45dc3894aded2bbaa79247782d769f6d1c0422c5
mokunshao <1627510957@qq.com>
2019-04-03 04:28:29 +0800

    备份

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-// import Layout from "antd/lib/layout";
-// import Row from "antd/lib/row";
-// import Col from "antd/lib/col";
-import {Layout,Row,Col} from 'antd'
-import './Header.scss'
+import './Header.scss';
+import {Link} from 'react-router-dom';
+import {Layout, Row, Col, Divider, Menu, Icon} from 'antd';
+
 
 export default function Header() {
   return (
@@ -12,7 +11,14 @@ export default function Header() {
         <Col md={6} xs={24}>
           <h1 className='headerLogo'>cNode</h1>
         </Col>
-        <Col md={18} xs={0}/>
+        <Col md={18} xs={0}>
+          <Divider type="vertical" className='headerDivider'/>
+          <Menu mode='horizontal' theme='light' className='headerMenu'>
+            <Menu.Item><Link to='/index'><Icon type='home'/>首页</Link></Menu.Item>
+            <Menu.Item><Link to='/tutorial'><Icon type='book'/> 教程</Link></Menu.Item>
+            <Menu.Item><Link to='/about'><Icon type='info-circle-o'/>关于</Link></Menu.Item>
+          </Menu>
+        </Col>
       </Row>
     </Layout.Header>
   );
