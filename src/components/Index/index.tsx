8110fedc678d6129ed7d47ef5436048ca22716af
mokunshao <1627510957@qq.com>
2019-04-08 23:07:09 +0800

    给导航栏添加选中状态

--- src/components/Index/index.tsx
+++ src/components/Index/index.tsx
@@ -1,22 +1,22 @@
-import React, {useState} from "react";
-import {Row, Col} from "antd";
+import React from "react";
+import { Row, Col } from "antd";
 import "./index.scss";
 import IndexMenu from "../IndexMenu";
 import IndexList from "../IndexList";
 
-function Index(props:any) {
+function Index(props: any) {
   window.scrollTo && window.scrollTo(0, 0);
   return (
     <Row className="contentWrapper">
       <Col md={6} xs={0}>
-        <IndexMenu className="indexMenu" mode="vertical"/>
+        <IndexMenu className="indexMenu" mode="vertical" />
       </Col>
       <Col md={0} xs={24}>
-        <IndexMenu className="indexMenu" mode="horizontal"/>
-        <IndexList className="indexList" section={props.match.params.section}/>
+        <IndexMenu className="indexMenu" mode="horizontal" />
+        <IndexList className="indexList" section={props.match.params.section} />
       </Col>
       <Col md={18} xs={0}>
-        <IndexList className="indexList" section={props.match.params.section}/>
+        <IndexList className="indexList" section={props.match.params.section} />
       </Col>
     </Row>
   );
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {Menu, Icon} from 'antd';
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Menu, Icon } from "antd";
 
 interface Props {
   mode: any;
+  pathname: string;
   className?: string;
 }
 
-function Nav(props:Props) {
+function Nav(props: Props) {
+  const [currentTab, changeCurrentTab] = useState("index");
+  useEffect(() => {
+    changeCurrentTab(props.pathname.split("/")[1]);
+  }, [props.pathname]);
   return (
     <Menu
       mode={props.mode}
-      theme='light'
+      theme="light"
       className={props.className}
+      selectedKeys={[currentTab]}
     >
-      <Menu.Item><Link to='/index/all'><Icon type='home'/>首页</Link></Menu.Item>
-      <Menu.Item><Link to='/tutorial'><Icon type='book'/> 教程</Link></Menu.Item>
-      <Menu.Item><Link to='/about'><Icon type='info-circle-o'/>关于</Link></Menu.Item>
+      <Menu.Item key={"index"}>
+        <Link to="/index/all">
+          <Icon type="home" />
+          首页
+        </Link>
+      </Menu.Item>
+      <Menu.Item key={"tutorial"}>
+        <Link to="/tutorial">
+          <Icon type="book" /> 教程
+        </Link>
+      </Menu.Item>
+      <Menu.Item key={"about"}>
+        <Link to="/about">
+          <Icon type="info-circle-o" />
+          关于
+        </Link>
+      </Menu.Item>
     </Menu>
   );
 }
 
-export default Nav;
+export default withRouter((props: any) => {
+  return (
+    <Nav
+      mode={props.mode}
+      className={props.className}
+      pathname={props.location.pathname}
+    />
+  );
+});
